b169c76fec5f8ae183aa78c0dec69f69f8e06875
Andrei Matveyeu <andrei@ideabulbs.com>
2014-06-14 20:14:16 +0200

    improved algorithm for calculating repetition weights

--- libschedule/items.py
+++ libschedule/items.py
@@ -1,15 +1,23 @@
 import datetime
 import time
 
+
 class Repetition(object):
-    repetition_timestamp = None
+
+    def __init__(self):
+        self.repetition_timestamp = None
+
 
 class RepeatableItem(object):
 
-    next_repetition_timestamp = None
-    repetitions = []
-    
+    def __init__(self):
+        self.repetitions = []
+        self.next_repetition_timestamp = None
+        
     def get_priority(self, repetition_time=None):
+        """
+        Calculate item repetition priority at the given time
+        """
         if repetition_time is None:
             repetition_time = time.time()
 
@@ -22,28 +30,39 @@ class RepeatableItem(object):
         if time_delta < 0:
             return None
         
-        # if no repetitions made yet, the item will have priority 10
+        # if no repetitions made yet, the item will have priority 1.0
         if len(self.repetitions) == 0:
-            return 1.0
+            return 100
 
+        # if only one repetition has been made, priority will be 10
         if len(self.repetitions) == 1:
-            return 10
+            return 75
 
         # if the item has no age, the item will have priority 0
         age = self.get_age()
         if age == 0:
             return 0
-        
+
+        age = age / float(86400)
         waiting = time_delta / float(86400)
         
-        priority = float(1.0 / age * 86400 + 1.0 / len(self.repetitions)) + waiting * 86400
+        priority = (float(waiting) / age * 100.0) +  30.0 / len(self.repetitions)
+        if priority < 0:
+            return 0
+        
         return priority
 
     def get_age(self):
+        """Get item age
+
+        Age is determined as the time difference between the first and the last repetition of the item
+        """
         if len(self.repetitions) == 0:
             return None
+        
         elif len(self.repetitions) == 1:
             last_repetition_timestamp = time.time()
+            
         else:
             last_repetition_timestamp = self.repetitions[-1].repetition_timestamp
         age = last_repetition_timestamp - self.repetitions[0].repetition_timestamp
--- libschedule/tests/test_items.py
+++ libschedule/tests/test_items.py
@@ -1,9 +1,10 @@
 import libschedule
 import time
-
+import datetime
 
 class TestRepeatableItem(object):
-    def test_age(self):
+
+    def test_priority_simple(self):
         
         # non-scheduled item has no priority
         ri = libschedule.RepeatableItem()
@@ -11,18 +12,66 @@ class TestRepeatableItem(object):
 
         # a scheduled item with no repetitions has priority 1.0
         ri.next_repetition_timestamp = time.time()
-        assert ri.get_priority() == 1.0
+        assert ri.get_priority() == 100
 
         # a scheduled item with one repetition has priority 10.0
         repetition = libschedule.Repetition()
         repetition.repetition_timestamp = time.time() - 86400 * 3
         ri.repetitions.append(repetition)
+        assert ri.get_priority() == 75
 
-        assert ri.get_priority() == 10.0
-
+        # an item with two repetitions (3 and 1 days ago) will have a priority of ca 1.0
         repetition = libschedule.Repetition()
         repetition.repetition_timestamp = time.time() - 86400
         ri.repetitions.append(repetition)
+        assert 14.95 < ri.get_priority() < 15.05
 
-        print ri.get_priority()
-        assert 0.99 < ri.get_priority() < 1.01
+    def create_item(self, age, reps, wait):
+        ri = libschedule.RepeatableItem()
+        now = time.time()
+        last_due = now - wait * 86400
+        ri.next_repetition_timestamp = last_due
+
+        from_time = last_due - age * 86400
+        first_rep = libschedule.Repetition()
+        first_rep.repetition_timestamp = from_time
+        ri.repetitions.append(first_rep)
+
+        if reps > 2:
+            for rep in range(reps-2):
+                r = libschedule.Repetition()
+                ri.repetitions.append(r)
+
+        if reps > 1:
+            last_rep = libschedule.Repetition()
+            last_rep.repetition_timestamp = last_due
+            ri.repetitions.append(last_rep)
+            
+        return ri
+
+    def test_priority_complex(self):
+        #           Age, Reps, Wait, Priority
+        matrix = [
+                    (3,  1,    1,    75),
+                    (3,  2,    2,    82),
+                    (3,  3,    2,    76),
+                    (5,  2,    1,    35),
+                    (5,  2,    2,    55),
+                    (5,  2,    3,    75),
+                    (12,  3,    20,  175),
+                    (1000,  35, 100, 11),
+
+                 ]
+        print "Age\tReps\tWait\tPrio"
+        for item in matrix:
+            current_item = self.create_item(item[0], item[1], item[2])
+            actual_priority = current_item.get_priority()
+            expected_priority = item[3]
+            allowed_inaccuracy = 0.02
+            lower_bound = expected_priority - expected_priority * allowed_inaccuracy
+            upper_bound = expected_priority + expected_priority * allowed_inaccuracy
+
+            print "%d\t%d\t%d\t%.2f" % (item[0], item[1], item[2], actual_priority)
+            #print "%.2f\t%.2f\t%.2f" % (lower_bound, actual_priority, upper_bound)
+            assert lower_bound <= actual_priority <= upper_bound
+            
